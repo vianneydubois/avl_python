9758ba964139517025d1ff8cbe4c0030545705bf
vianneydubois <vianneydubois@hotmail.fr>
2021-06-01 22:21:55 +0200

    shell_test_2.py is working. But the output file must be deleted before the program is run again to avoid a 'File exists' situation with AVL that makes the process crash

--- shell_test_2.py
+++ shell_test_2.py
@@ -1,15 +1,35 @@
 import os.path
 import subprocess as sp
 
+load = 'LOAD'
+input_geometry = '/Users/vianneydubois/Desktop/AVL_VLM/test_gen.avl'
+oper = 'OPER'
+alpha = 'A C 0.5'
+execute = 'X'
+stab = 'ST'
+output_st = '/Users/vianneydubois/Desktop/AVL_VLM/test_out.txt'
+back = ''
+quit = 'QUIT'
+
+command_list = [load,
+                input_geometry,
+                oper,
+                alpha,
+                execute,
+                stab,
+                output_st,
+                back,
+                quit]
+
+command_string = '\n'.join(command_list)
+command_string = command_string.encode('ascii')
+
+# AVL_FOLDER_PATH = '~/Desktop/AVL_VLM'
 AVL_FOLDER_PATH = '.'
 AVL_EXE_NAME = 'avl335'
 avl_path = os.path.join(AVL_FOLDER_PATH, AVL_EXE_NAME)
 
+avl_ps = sp.Popen([avl_path], stdin=sp.PIPE, stdout=None, stderr=None)
+avl_ps.communicate(input=command_string)
 
-def avl_command(cmd):
-    return sp.run(cmd, check=True, shell=True)
 
-
-result = avl_command([avl_path])
-
-#result = avl_command(['LOAD', '/Users/vianneydubois/Desktop/AVL_VLM/test_gen.avl'])
